eec79c8dd59fe95457b2fbf3b792679a84c3b0dd
israelrp <israel@novatek.me>
2014-08-12 20:11:34 -0500

    Inicio del modulo de facturas de ventas

---
+++ AIPos.WebLayer/Controllers/VentasFacturarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIPos.WebLayer.Controllers
+{
+    public class VentasFacturarController : Controller
+    {
+        //
+        // GET: /VentasFacturar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
